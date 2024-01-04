677246f598994a8916ec2910c141b2a24a9fb137
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-04 15:47:26 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/admin",
-        destination: "https://app.forestry.io/login",
-        permanent: true,
-        basePath: false,
-      },
-    ];
-  },
   // Append the default value with md extensions
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx", "html"],
+  pageExtensions: ["ts", "tsx", "js", "jsx", "html"],
   reactStrictMode: true,
   trailingSlash: false,
   images: {
