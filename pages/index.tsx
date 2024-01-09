24ba270b3ed3690036af0ece71a4c1ea3189f220
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-09 06:13:46 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ import AppHead from "@/components/AppHead";
 export const meta = {
   description:
     "Angel Tomas Concha Layme is a full-stack developer from Arequipa - Peru. He is passionate about writing code and developing web applications to solve real-life challenges.",
-  author: "Angel Concha",
+  author: "Angel Tomas Concha Layme",
   type: "website",
   ogImage: `${process.env.NEXT_PUBLIC_URL}/angeltomas-dev-og-new.png`,
   siteName: "Angel Concha",
@@ -25,11 +25,11 @@ const Home: NextPage = () => {
   return (
     <>
       <AppHead
-        title="Angel Concha | Full-stack Developer"
+        title="Angel Tomas Concha Layme | Full-stack Developer"
         url={`${process.env.NEXT_PUBLIC_URL}`}
         meta={meta}
       />
-      <Loader>TomasLive.dev</Loader>
+      <Loader>AngelTomas.dev</Loader>
       <div className="bg-bglight dark:bg-bgdark overflow-hidden">
         <div className="selection:bg-marrsgreen selection:text-bglight dark:selection:bg-carrigreen dark:selection:text-bgdark">
           <SkipToMain />
