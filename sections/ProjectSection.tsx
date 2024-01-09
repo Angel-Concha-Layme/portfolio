98d4f087f6fe0133336a5f1c07117ff508d86151
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-09 05:15:33 -0500

    Update ProjectSection.tsx

--- sections/ProjectSection.tsx
+++ sections/ProjectSection.tsx
@@ -8,19 +8,17 @@ import { useSection } from "context/section";
 import useOnScreen from "hooks/useOnScreen";
 import useScrollActive from "hooks/useScrollActive";
 
-
 import fakestoreApi from "public/projects/fakestore-api.webp";
 import teamcode from "public/projects/teamcode-runner.webp";
 import haddopSearch from "public/projects/hadoop-search.webp";
 import noteBoard from "public/projects/noteboard.webp";
 
-
 const ProjectSection: React.FC = () => {
   const { theme } = useTheme();
 
   const sectionRef = useRef<HTMLDivElement>(null);
-
   const elementRef = useRef<HTMLDivElement>(null);
+
   const isOnScreen = useOnScreen(elementRef);
 
   // Set active link for project section
@@ -31,6 +29,10 @@ const ProjectSection: React.FC = () => {
   }, [projectSection, onSectionChange]);
 
   return (
+    <div
+    ref={sectionRef}
+    className="project-panel bg-white dark:bg-[#1B2731] relative"
+  >
     <section ref={sectionRef} id="projects" className="section">
       <div className="project-title text-center">
         <RoughNotation
@@ -61,7 +63,9 @@ const ProjectSection: React.FC = () => {
         </a>
       </div>
     </section>
+    </div>
   );
+  
 };
 
 const projects = [
@@ -77,8 +81,17 @@ const projects = [
         className="transition-transform duration-500 hover:scale-110 object-cover"
       />
     ),
-    desc: "Fake Store API is a service that offers CRUD operations, authentication using JWT, and advanced features such as filtering and pagination. This API is developed using Spring Boot, MySQL, Spring Security, and Spring Data JPA. The frontend is built with Astro. The entire system is deployed on Google Cloud.", 
-    tags: ["Java", "Spring Boot", "Spring Security", "Spring Data JPA", "MySQL", "Google Cloud Platform", "Postman", "Swagger"],
+    desc: "Fake Store API is a service that offers CRUD operations, authentication using JWT, and advanced features such as filtering and pagination. This API is developed using Spring Boot, MySQL, Spring Security, and Spring Data JPA. The frontend is built with Astro. The entire system is deployed on Google Cloud.",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "Spring Security",
+      "Spring Data JPA",
+      "MySQL",
+      "Google Cloud Platform",
+      "Postman",
+      "Swagger",
+    ],
     liveUrl: "https://fake-store-api-docs.vercel.app/",
     codeUrl: "https://github.com/Angel-Concha-Layme/Fake-Store-API",
     bgColor: "bg-[#B4BEE0]",
@@ -96,7 +109,18 @@ const projects = [
       />
     ),
     desc: "Web platform for code execution in multiple languages (Java, Python, JavaScript), utilizing a microservices architecture. Each language-specific service is built using Express for JavaScript, Flask for Python, and Spring Boot for Java, orchestrated by a primary Spring Boot service. The frontend is developed with Vue.js and incorporates Ace Editor for syntax highlighting and autocompletion. The entire application is deployed on Google Cloud Platform.",
-    tags: ["Java", "Python", "JavaScript", "Spring Boot", "Flask", "Express", "Vue.js", "Ace Editor", "Microservices", "Google Cloud Platform"],
+    tags: [
+      "Java",
+      "Python",
+      "JavaScript",
+      "Spring Boot",
+      "Flask",
+      "Express",
+      "Vue.js",
+      "Ace Editor",
+      "Microservices",
+      "Google Cloud Platform",
+    ],
     liveUrl: "https://angel-concha-layme.github.io/FrontSampleTeamCode/",
     codeUrl: "https://github.com/Angel-Concha-Layme/FrontSampleTeamCode",
     bgColor: "bg-[#B4BEE0]",
@@ -133,12 +157,23 @@ const projects = [
       />
     ),
     desc: "The NoteBoard Challenge is a versatile and user-friendly application for managing notes. The first phase allows users to create, edit, delete, archive, and unarchive notes, as well as list active and archived notes. The second phase introduces the ability to add or remove categories from notes and filter them by category. The backend is developed using Spring Boot, Spring Data JPA, Flyway, Lombok, and MySQL, and is deployed on Google Cloud Platform. The frontend, crafted with Vue.js, Vite, and Node.js, offers a smooth and responsive user experience, deployed using Vercel.",
-    tags: ["Spring Boot", "Spring Data JPA", "Flyway", "Lombok", "MySQL", "Java", "Vue.js", "Vite", "Node.js", "Google Cloud Platform", "Vercel"],
+    tags: [
+      "Spring Boot",
+      "Spring Data JPA",
+      "Flyway",
+      "Lombok",
+      "MySQL",
+      "Java",
+      "Vue.js",
+      "Vite",
+      "Node.js",
+      "Google Cloud Platform",
+      "Vercel",
+    ],
     liveUrl: "https://noteboard-frontend.vercel.app/",
     codeUrl: "https://github.com/Angel-Concha-Layme/Noteboard",
     bgColor: "bg-[#B4BEE0]",
-  }
-  
+  },
 ];
 
 export default ProjectSection;
